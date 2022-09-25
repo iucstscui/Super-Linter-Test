1b59fc2dba24189b04fb6e9cbff70f41ffc5cfeb
iucstscui <icchicchicchi@gmail.com>
2022-09-25 22:42:19 +0900

    sample code commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(100);
+        }
+
+        static void FizzBuzz(int max)
+        {
+            for (int i = 1; i <= max; ++i)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
